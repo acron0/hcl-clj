dd4f41bd492da05cc2cfce4923384f81c2330a0d
Antony Woods <acron1@gmail.com>
2022-05-07 17:53:59 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.acron0/hcl-clj "0.1.0-SNAPSHOT"
+(defproject org.clojars.acron0/hcl-clj "0.1.0"
   :description "Convert HashiCorp HCL to Clojure hash map"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
