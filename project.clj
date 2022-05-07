5693226d3a82cf91c706daaa3c6134418fabbfef
Antony Woods <acron1@gmail.com>
2022-05-07 11:18:40 +0100

    Tidy the project file

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject hcl-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Convert HashiCorp HCL to Clojure hash map"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.bertramlabs.plugins/hcl4j "0.4.0"]]
-  :main ^:skip-aot hcl-clj.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]
+                 ["snapshots" {:url "https://clojars.org/repo"
+                               :creds :gpg}]])
